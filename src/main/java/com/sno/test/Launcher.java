4ff4ba87cd54484347facb71b9a37f3c98891559
SnoFlak <mathiasasletten@gmail.com>
2024-10-26 20:04:59 -0500

    initial setup EP1

---
+++ src/main/java/com/sno/test/Launcher.java
@@ -0,0 +1,10 @@
+package com.sno.test;
+
+import org.lwjgl.Version;
+
+public class Launcher {
+
+    public static void main(String[] args){
+        System.out.println(Version.getVersion());
+    }
+}
