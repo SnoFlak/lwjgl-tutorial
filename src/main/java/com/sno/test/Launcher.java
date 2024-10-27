f03a83618ae2f2350304d90ead43a253fdbd5801
SnoFlak <mathiasasletten@gmail.com>
2024-10-26 20:37:37 -0500

    Window Manager

--- src/main/java/com/sno/test/Launcher.java
+++ src/main/java/com/sno/test/Launcher.java
@@ -1,10 +1,19 @@
 package com.sno.test;
 
+import com.sno.core.WindowManager;
 import org.lwjgl.Version;
 
 public class Launcher {
 
     public static void main(String[] args){
         System.out.println(Version.getVersion());
+        WindowManager window = new WindowManager("SNOFLAK ENGINE", 0, 0, false);
+        window.init();
+
+        while(!window.windowShouldClose()) {
+            window.update();
+        }
+
+        window.cleanup();
     }
 }
